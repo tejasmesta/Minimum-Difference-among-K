8809bc6e3223973b71350c6b4642083d3175b16c
Tejas Mesta <tejasmesta@gmail.com>
2022-08-14 04:39:43 -0700

    Add files via upload

---
+++ Minimum Difference among K.java
@@ -0,0 +1,23 @@
+class Solution
+{
+    long minDiff(long a[] ,int N,int K)
+    {
+        Arrays.sort(a);
+        
+        long ans = Integer.MAX_VALUE;
+        
+        for (int i=0 ; i <= N-K ; i++){
+            long min  = a[i];
+            long max = a[i+K-1];
+            
+            long d = max- min;
+            
+            if (d < ans){
+                ans = d;
+            }
+        }
+        
+        return ans;
+        
+    }
+}
